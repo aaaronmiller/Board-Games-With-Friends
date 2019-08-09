1dcd942363d21b2780ec1bce92deb50c815ef235
Aaron Miller <aaaronmiller@gmail.com>
2019-08-09 11:14:18 -0700

    .

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -44,26 +44,26 @@ export default class DashBoard extends Component {
     render() {
         return (
             <MDBContainer>
-                <div>
-                    {/* <h1 className="text-white">Upcoming Games
-                    <MDBBtn color="#d50000 red accent-4" style={{ color: "white", marginLeft: "340px" }} href="#"  onClick={this.handleSubmit}>Create a Game</MDBBtn></h1> */}
-                    {/* <div className="d-flex flex-row flex-wrap"> */}
-                    {this.state.grabbedGames.map((data)=> 
+            <div>
+            <h1 className="text-white">Upcoming Games
+            <MDBBtn color="#d50000 red accent-4" style={{ color: "white", marginLeft: "340px" }} href="#"  onClick={this.handleSubmit}>Create a Game</MDBBtn></h1>
+            <div className="d-flex flex-row flex-wrap">
+            {this.state.grabbedGames.map((data)=> 
                 (<GameCard  key={data.id} eventTitle={data.eventTitle} description={data.description} location={data.location} capacity ={data.capacity}/>))}
-
-                    {/* </div>x x */}
+                
+                    
                 </div>
                 {/* <div>
                 <h1 className="text-white">Hosted Games</h1>
                     <div className="d-flex flex-row flex-wrap">
-                        <GameCard />
-                        <GameCard />
-                        <GameCard />
-                        <GameCard />
-                        <GameCard />
-                    </div>
-                </div> */}
-            </MDBContainer>
-        )
-    }
+                    <GameCard />
+                    <GameCard />
+                    <GameCard />
+                    <GameCard />
+                    <GameCard />
+                </div>*/}
+                </div> 
+                </MDBContainer>
+                )
+            }
 }
--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -7,53 +7,54 @@ class CardExample extends Component {
   state = {
     //creator toggle
     isOwner: false, 
-    //populated from user table:
+    // populated from user table:
 
-    // //populated from event table:
-    // "id": 2,
-    // "description": "this desc",
-    // "location": "jersy",
-    // "capacity": 8,
-    // "phone": 1234123,
-    // "createdAt": "2019-08-09T05:22:28.000Z",
-    // "updatedAt": "2019-08-09T05:22:28.000Z",
-    // "userName": null,
-    // "UserId": null,
-    // "User": null
+    //populated from event table:
+    id: "",
+    description: "",
+    location: "",
+    capacity: "",
+    phone: "",
+    createdAt: "",
+    updatedAt: "",
+    userName: "",
+    UserId: "",
+    User: "",
     // Below are from the game table, not event table
-    gameImage: "http://miro.medium.com/max/1838/1*fcOemY1xrwLcWUnX8l8g8Q.jpeg",
+    gameImage: "",
   };
 
-  // // componentDidMount() {
-  // //   this.loadEvents();
-  // // }
+  componentDidMount() {
+    this.loadEvents();
+  };
 
-  // loadEvents = () => {
-  //   API.getAllEvents()
-  //     .then(res =>
-  //       this.setState({ gameObj: res.data })
-  //     )
-  //     .catch(err => console.log(err));
-  // };
+  loadEvents = () => {
+    API.getAllEvents()
+      .then(res =>
+        this.setState({ gameObj: res.data })
+      )
+      .catch(err => console.log(err));
+  };
 
-  // deleteEvent = id => {
-  //   API.deleteEvent(id)
-  //     .then(res => this.loadEvents())
-  //     .catch(err => console.log(err));
-  // };
+  deleteEvent = id => {
+    API.deleteEvent(id)
+      .then(res => this.loadEvents())
+      .catch(err => console.log(err));
+  };
 
-  // handleInputChange = event => {
-  //   const { name, value } = event.target;
-  //   this.setState({
-  //     [name]: value
-  //   });
-  // };
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
   
   render() {
     return (
       <MDBCol md="4">
+      dog
         <MDBCard className="text-white bg-light" style={{ textAlign: "center", margin: "30px" }}>
-          <MDBCardImage className="img-fluid" src={this.gameImage} waves />
+          <MDBCardImage className="img-fluid" src={this.props.gameImage} waves />
           <MDBCardBody>
             <MDBCardTitle>{this.props.eventTitle}</MDBCardTitle>
             <MDBCardText>
@@ -66,8 +67,8 @@ class CardExample extends Component {
               {/* <span>Curerent players: {CardExample.signedInPlayers}</span> */}
             </MDBCardText>
             <MDBBtn color="#1565c0 blue darken-3" style={{ color: "white" }} href="#"  onClick={this.handleSubmit}>Join</MDBBtn>
-            {this.state.isOwner ? (<MDBBtn color="#1565c0 blue darken-3" style={{ color: "white" }} href="#"  onClick={this.handleSubmit}>Delete</MDBBtn>) : ("")}
-            {this.state.isOwner ? (<MDBBtn color="#1565c0 blue darken-3" style={{ color: "white" }} href="#"  onClick={this.handleSubmit}>Edit</MDBBtn>): ("")}
+            {this.props.isOwner ? (<MDBBtn color="#1565c0 blue darken-3" style={{ color: "white" }} href="#"  onClick={this.handleSubmit}>Delete</MDBBtn>) : ("")}
+            {this.props.isOwner ? (<MDBBtn color="#1565c0 blue darken-3" style={{ color: "white" }} href="#"  onClick={this.handleSubmit}>Edit</MDBBtn>): ("")}
           </MDBCardBody>
         </MDBCard>
       </MDBCol>
