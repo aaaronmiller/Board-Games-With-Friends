fc49576d2dac0b21dd075f28ad64685c1dd1f58d
Aaron Miller <aaaronmiller@gmail.com>
2019-08-11 16:41:21 -0700

    profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,82 @@
-import React from 'react'
+import React from "react";
+import {  MDBContainer, MDBRow, MDBCol, MDBIcon, MDBBtn, MDBInput } from "mdbreact";
 
-export default function Profile() {
-    return (
-        <div>
-            
-        </div>
-    )
+const ContactPage = () => {
+  return (
+    <MDBContainer className="profilePage">
+      <h2 className="h1-responsive font-weight-bold text-center my-5 white-text">
+        My Profile
+      </h2>
+      <MDBRow>
+        <MDBCol md="9" className="md-0 mb-5 white-text" >
+          <form>
+            <MDBRow>
+              <MDBCol md="6">
+                <div className="md-form mb-0">
+                  <MDBInput type="text" id="placeholder" label="Your name" />
+                </div>
+              </MDBCol>
+              <MDBCol md="6">
+                <div className="md-form mb-0">
+                  <MDBInput
+                    type="email"
+                    id="placeholder"
+                    label="Your email"
+                  />
+                </div>
+              </MDBCol>
+            </MDBRow>
+            <MDBRow>
+              <MDBCol md="12">
+                <div className="md-form mb-0">
+                  <MDBInput type="text" id="placeholder" label="Password" />
+                </div>
+              </MDBCol>
+            </MDBRow>
+            <MDBRow>
+              <MDBCol md="12">
+                <div className="md-form mb-0">
+                  <MDBInput type="text" id="placeholder" label="Your Favorite Games" />
+                </div>
+              </MDBCol>
+            </MDBRow>
+            {/* <MDBRow>
+              <MDBCol md="12">
+                <div className="md-form mb-0">
+                  <MDBInput
+                    type="textarea"
+                    id="placeholder"
+                    label="Your favorite Games"
+                  />
+                </div>
+              </MDBCol>
+            </MDBRow> */}
+          </form>
+          <div className="text-center text-md-left">
+            <MDBBtn color="primary" size="md">
+              Update
+            </MDBBtn>
+          </div>
+        </MDBCol>
+        <MDBCol md="3" className="text-center">
+          <ul className="list-unstyled mb-0">
+            <li>
+              <MDBIcon icon="map-marker-alt" size="2x" className="blue-text" />
+              <p>Seattle, WA 98114, USA</p>
+            </li>
+            <li>
+              <MDBIcon icon="phone" size="2x" className="blue-text mt-4" />
+              <p>+ 01 234 567 89</p>
+            </li>
+            <li>
+              <MDBIcon icon="envelope" size="2x" className="blue-text mt-4" />
+              <p>youremail@example.com</p>
+            </li>
+          </ul>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
 }
+
+export default ContactPage;
