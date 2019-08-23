5d7b8604cdf48af30ef7ec8732dddaf01f7c683e
Aaron Miller <aaaronmiller@gmail.com>
2019-08-22 17:03:06 -0700

    new server

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 // const url = "https://arcane-spire-45572.herokuapp.com";
-const url = "http://localhost:8080";
+const url = "https://limitless-eyrie-66892.herokuapp.com";
+
+// const url = "http://localhost:8080";
 
 export default {
   // Event API's  
